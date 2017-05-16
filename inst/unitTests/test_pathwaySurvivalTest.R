9fc43947e39c5865c379093198ee0c0a739a3476
Paolo Martini <paolo.cavei@gmail.com>
2017-05-16 10:05:38 +0200

    Test for pathway survival test.

---
+++ inst/unitTests/test_pathwaySurvivalTest.R
@@ -0,0 +1,33 @@
+library(graphite)
+library(survival)
+data(exp)
+data(ann)
+
+set.seed(1234)
+
+samples <- intersect(colnames(exp),row.names(ann))
+
+exp <- exp[,samples, drop=FALSE]
+ann <- ann[samples,, drop=FALSE]
+
+time <- computeDays(ann[,1:2])
+events <- as.numeric(ann[,3])
+isto <- ann[,"Isto"]
+grade <- ann[,"Grado"]
+
+survAnnot <- data.frame(days=time, status=events, grade=grade,
+                        isto=isto, row.names=samples)
+
+k <- pathways("hsapiens", "kegg")
+k <- convertIdentifiers(k, "entrez")
+graph <- pathwayGraph(k[["Pathways in cancer"]])
+
+genes <- intersect(graph::nodes(graph), row.names(exp))
+graph <- graph::subGraph(genes, graph)
+expr <- exp[genes, , drop=FALSE]
+
+test_pathwaySurvivalTest <- function(){
+  set.seed(1234)
+  test <- pathwaySurvivalTest(expr, survAnnot, graph, maxPCs=3)
+}
+
