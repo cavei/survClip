cf9d5eedc2b70cf50ee32ee516e76b9141aa144b
Paolo Martini <paolo.cavei@gmail.com>
2017-05-29 14:50:14 +0200

    Set seed to compare procedure in whole pathway.

--- R/pathwaySurvivalTest.R
+++ R/pathwaySurvivalTest.R
@@ -27,11 +27,17 @@ pathwaySurvivalTest <- function(expr, survAnnot, graph, nperm=100, perc=0.8, for
   days   <- survAnnot$days
   events <- survAnnot$status
 
+  set.seed(1234)
   gtpvalue           <- globaltest::p.value(globaltest::gt(Surv(days, events==1), alternative=t(expr), permutations=nperm))
+  set.seed(1234)
   pcspvalue          <- survCoxOnAllPCs(genes, expr, perc, survAnnot, pc2class=TRUE, robust=FALSE, maxPCs=maxPCs)
+  set.seed(1234)
   pcspvalueCov       <- survCoxOnAllPCs(genes, expr, perc, survAnnot, pc2class=TRUE, robust=FALSE, shrink=FALSE, cliques=cliques, maxPCs=maxPCs)
+  set.seed(1234)
   pcspvalueCovAlways <- survCoxOnAllPCs(genes, expr, perc, survAnnot, pc2class=TRUE, robust=FALSE, shrink=TRUE,  cliques=cliques, maxPCs=maxPCs)
+  set.seed(1234)
   pcspvalueCovAlwaysNT <- survCoxOnAllPCs(genes, expr, perc, survAnnot, pc2class=TRUE, robust=FALSE, shrink=FALSE, cliques=cliques, maxPCs=maxPCs, useTopology = FALSE)
+  set.seed(1234)
   pcspvalueCovAlwaysNT.shrink <- survCoxOnAllPCs(genes, expr, perc, survAnnot, pc2class=TRUE, robust=FALSE, shrink=TRUE,  cliques=cliques, maxPCs=maxPCs, useTopology = FALSE)
   
   return(list(gtPvalue=gtpvalue, pcsPvalue=pcspvalue, pcsPvalueCov=pcspvalueCov, pcsPvalueCovAlways=pcspvalueCovAlways, noTopo=pcspvalueCovAlwaysNT, noTopoShrinked=pcspvalueCovAlwaysNT.shrink))
